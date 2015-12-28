eea1d748302ea45af3a03250be90cd53f9ea5f93
ChanneDesign <channebertrand@gmail.com>
2015-12-28 16:24:23 -0500

    some changes

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
   	@contact_form = ContactForm.new
-  	@video_cats = VideoCategory.all
+  	@video_cats = VideoCategory.order("position ASC")
   	@photo_cats = PhotoCategory.all
   end
 end
--- app/controllers/video_categories_controller.rb
+++ app/controllers/video_categories_controller.rb
@@ -2,12 +2,12 @@ class VideoCategoriesController < ApplicationController
 	before_action :authenticate_admin!
 	def new
 		@video_cat = VideoCategory.new
-		@video_cats = VideoCategory.all
+		@video_cats = VideoCategory.order("position ASC")
 	end
 
 	def create
 		@video_cat = VideoCategory.new(video_category_params)
-		@video_cats = VideoCategory.all
+		@video_cats = VideoCategory.order("position ASC")
 
 		if @video_cat.save
 			redirect_to admins_video_path, notice: "Category created successfully"
@@ -17,7 +17,7 @@ class VideoCategoriesController < ApplicationController
 	end
 
 	def edit
-		@video_cats = VideoCategory.all
+		@video_cats = VideoCategory.order("position ASC")
 		@video_cat = VideoCategory.find(params[:id])
 	end
 
