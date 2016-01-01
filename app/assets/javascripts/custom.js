fe72f70c57e6f237bdbc517839ea199a52f7c708
ChanneDesign <channebertrand@gmail.com>
2016-01-01 09:05:39 -0500

    some changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -4,8 +4,19 @@ $(document).ready(function() {
 		$(".fullHeight_video, .fullHeight_photo ").css("min-height", $(window).height());
 	}
 
+	//Detect if touch device
+	function isTouchSupported() {
+	    var msTouchEnabled = window.navigator.msMaxTouchPoints;
+	    var generalTouchEnabled = "ontouchstart" in document.createElement("div");
+	 
+	    if (msTouchEnabled || generalTouchEnabled) {
+	        return true;
+	    }
+	    return false;
+	}
+
 	//nav
-	if ($(window).width() > 990) {
+	if (($(window).width() > 990) && !(isTouchSupported()))  {
 		$(window).scroll(function() {
 			if ($(this).scrollTop() > 200) {
 				$("#nav").fadeIn("slow");
