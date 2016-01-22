582d04cc3f08c11831d5d88d01b0a23cdfaf1eb6
ChanneDesign <channebertrand@gmail.com>
2016-01-22 12:02:40 -0500

    some changes

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -56,8 +56,8 @@ $(document).ready(function() {
     
 	//Gallery Video init 
 	$('.gallery').featherlightGallery({
-		previousIcon: '<',
-    	nextIcon: '>',
+		previousIcon: '<img src="http://varareese.herokuapp.com/assets/left_arrow-0e8b9d9a893461b0050dc5a65f3d187ee03aae96b3ded430214b4de4ab13041a.png" class="featherlight_arrow" />',
+    	nextIcon: '<img src="http://varareese.herokuapp.com/assets/right_arrow-cb9a19584e7ffb752070594185c74f0153795e16195881a2c4c2251abfd106f7.png" class="featherlight_arrow" />',
     	galleryFadeIn: 100,         
 		galleryFadeOut: 300,
 		afterOpen: function() {
