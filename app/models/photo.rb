c65c3dad43ef5efc7a54d369fbc30f8f039ccaf0
ChanneDesign <channebertrand@gmail.com>
2016-02-08 10:53:49 -0500

    some changes

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,9 +2,9 @@ class Photo < ActiveRecord::Base
 	has_and_belongs_to_many :photo_categories
 	
 	acts_as_list
-	has_attached_file :photo, styles: { thumb: "330x215#" }, processors: [:papercrop, :thumbnail, :compression]
+	has_attached_file :photo, styles: { thumb: "215x215#" }, processors: [:papercrop, :thumbnail, :compression]
 	validates_attachment_content_type :photo, :content_type => /\Aimage\/.*\Z/
-	crop_attached_file :photo, :aspect => "16:9"
+	crop_attached_file :photo
 
 	private 
 		def downcase
