7035fd9e976dea6469b7b891ed86f1f2f1678ede
ChanneDesign <channebertrand@gmail.com>
2016-07-13 17:33:29 -0400

    removed n+1 queries

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,9 @@
 class HomeController < ApplicationController
   def index
   	@contact_form = ContactForm.new
-  	@videos = Video.all
-  	@video_cats = VideoCategory.order_by_position
+  	@video_cats = VideoCategory.order_by_position.includes(:videos)
   	@video_show = Video.reel
-  	@photo_cats = PhotoCategory.order_by_position
+  	@photo_cats = PhotoCategory.order_by_position.includes(:photos)
   	@photo_show = Photo.reel
   end
 end
